276835ee1fc6bbda7104e769a156396a3ddd1186
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-01 02:17:47 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,11 @@
-class CreateShows < ActiveRecord::migration
+class CreateShows < ActiveRecord::Migration
+  def change
+      create_table :shows do |t|
+        t.string :name
+        t.float :price
+        t.text :image_url
+        t.string :size
+        t.timestamps
+    end
+  end
 end
